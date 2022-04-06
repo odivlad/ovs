odivlad/ovs

commit e1ededf45f072c41295f1b441a6f106159ff191b

rhel: Enable ERSPAN features for RHEL 7.x
Enable ERSPAN on RHEL 7.x

Signed-off-by: Greg Rose <gvrose8192@gmail.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>
Acked-by: William Tu <u9012063@gmail.com>

acinclude.m4
@@ -531,7 +531,8 @@ AC_DEFUN([OVS_CHECK_LINUX_COMPAT], [
                         [OVS_GREP_IFELSE([$KSRC/include/net/ip_tunnels.h],
                                          [iptunnel_pull_offloads],
                         [OVS_GREP_IFELSE([$KSRC/include/net/dst_cache.h], [dst_cache],
-                                         [OVS_DEFINE([USE_UPSTREAM_TUNNEL])])])])
+                        [OVS_GREP_IFELSE([$KSRC/include/net/erspan.h], [erspan_md2],
+                                         [OVS_DEFINE([USE_UPSTREAM_TUNNEL])])])])])
 
   OVS_GREP_IFELSE([$KSRC/include/net/dst_cache.h], [dst_cache],
                   [OVS_DEFINE([USE_BUILTIN_DST_CACHE])])
@@ -866,6 +867,18 @@ AC_DEFUN([OVS_CHECK_LINUX_COMPAT], [
   OVS_GREP_IFELSE([$KSRC/net/ipv4/gre_demux.c],
                   [parse_gre_header],
                   [OVS_DEFINE([HAVE_DEMUX_PARSE_GRE_HEADER])])
+  OVS_GREP_IFELSE([$KSRC/include/uapi/linux/if_tunnel.h],
+                  [IFLA_GRE_ENCAP_DPORT])
+  OVS_GREP_IFELSE([$KSRC/include/uapi/linux/if_tunnel.h],
+                  [IFLA_GRE_COLLECT_METADATA])
+  OVS_GREP_IFELSE([$KSRC/include/uapi/linux/if_tunnel.h],
+                  [IFLA_GRE_IGNORE_DF])
+  OVS_GREP_IFELSE([$KSRC/include/uapi/linux/if_tunnel.h],
+                  [IFLA_GRE_FWMARK])
+  OVS_GREP_IFELSE([$KSRC/include/uapi/linux/if_tunnel.h],
+                  [IFLA_GRE_ERSPAN_INDEX])
+  OVS_GREP_IFELSE([$KSRC/include/uapi/linux/if_tunnel.h],
+                  [IFLA_GRE_ERSPAN_HWID])
 
   if cmp -s datapath/linux/kcompat.h.new \
             datapath/linux/kcompat.h >/dev/null 2>&1; then

datapath/linux/compat/ip6_gre.c
@@ -1693,30 +1693,29 @@ static void __net_exit ip6gre_exit_batch_net(struct list_head *net_list)
 }
 
 enum {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3,18,0)
+#ifndef HAVE_IFLA_GRE_ENCAP_DPORT
 	IFLA_GRE_ENCAP_TYPE = IFLA_GRE_FLAGS + 1,
 	IFLA_GRE_ENCAP_FLAGS,
 	IFLA_GRE_ENCAP_SPORT,
 	IFLA_GRE_ENCAP_DPORT,
 #endif
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,3,0)
+#ifndef HAVE_IFLA_GRE_COLLECT_METADATA
 	IFLA_GRE_COLLECT_METADATA = IFLA_GRE_ENCAP_DPORT + 1,
 #endif
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,8,0)
+#ifndef HAVE_IFLA_GRE_IGNORE_DF
 	IFLA_GRE_IGNORE_DF = IFLA_GRE_COLLECT_METADATA + 1,
 #endif
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,12,0)
+#ifndef HAVE_IFLA_GRE_FWMARK
 	IFLA_GRE_FWMARK = IFLA_GRE_IGNORE_DF + 1,
 #endif
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,15,0)
+#ifndef HAVE_IFLA_GRE_ERSPAN_INDEX
 	IFLA_GRE_ERSPAN_INDEX = IFLA_GRE_FWMARK + 1,
 #endif
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,16,0)
+#ifndef HAVE_IFLA_GRE_ERSPAN_HWID
 	IFLA_GRE_ERSPAN_VER = IFLA_GRE_ERSPAN_INDEX + 1,
 	IFLA_GRE_ERSPAN_DIR,
 	IFLA_GRE_ERSPAN_HWID,
 #endif
-
 };
 
 #define RPL_IFLA_GRE_MAX (IFLA_GRE_ERSPAN_HWID + 1)

datapath/linux/compat/ip_gre.c
@@ -814,25 +814,25 @@ out:
 }
 
 enum {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3,18,0)
+#ifndef HAVE_IFLA_GRE_ENCAP_DPORT
 	IFLA_GRE_ENCAP_TYPE = IFLA_GRE_FLAGS + 1,
 	IFLA_GRE_ENCAP_FLAGS,
 	IFLA_GRE_ENCAP_SPORT,
 	IFLA_GRE_ENCAP_DPORT,
 #endif
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,3,0)
+#ifndef HAVE_IFLA_GRE_COLLECT_METADATA
 	IFLA_GRE_COLLECT_METADATA = IFLA_GRE_ENCAP_DPORT + 1,
 #endif
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,8,0)
+#ifndef HAVE_IFLA_GRE_IGNORE_DF
 	IFLA_GRE_IGNORE_DF = IFLA_GRE_COLLECT_METADATA + 1,
 #endif
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,12,0)
+#ifndef HAVE_IFLA_GRE_FWMARK
 	IFLA_GRE_FWMARK = IFLA_GRE_IGNORE_DF + 1,
 #endif
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,15,0)
+#ifndef HAVE_IFLA_GRE_ERSPAN_INDEX
 	IFLA_GRE_ERSPAN_INDEX = IFLA_GRE_FWMARK + 1,
 #endif
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,16,0)
+#ifndef HAVE_IFLA_GRE_ERSPAN_HWID
 	IFLA_GRE_ERSPAN_VER = IFLA_GRE_ERSPAN_INDEX + 1,
 	IFLA_GRE_ERSPAN_DIR,
 	IFLA_GRE_ERSPAN_HWID,

commit e396e35a67e21007a2316b45a3dc2f2b8e05a9b9

datapath: compat: replace USE_UPSTREAM_TUNNEL with new macro for erspan
This patch replaces USE_UPSTREAM_TUNNEL macro in erspan.h with new
USE_BUILTIN_ERSPAN_MD2 macro.
This is needed to fix broken udp_tnl RX offload (VXLAN & GENEVE)
in old RHEL 7 kernels, which do not have include/net/erspah.h (3.10.x).

Prior to this patch RX traffic in tcpdump:

19:19:00.223575 IP 10.1.0.105.60228 > 10.1.0.106.6081: Geneve, Flags [none], vni 0x0: IP 1.1.1.5.targus-getdata1 > 1.1.1.6.44652: Flags [.], seq 40722105:40723553, ack 38, win 64, options [nop,nop,TS val 2491722176 ecr 151867193], length 1448

With this patch applied:

19:05:13.500646 IP 10.1.0.105.51432 > 10.1.0.106.6081: Geneve, Flags [none], vni 0x0: IP 1.1.1.5.targus-getdata1 > 1.1.1.6.54874: Flags [.], seq 185753153:185818313, ack 38, win 64, options [nop,nop,TS val 2490895458 ecr 151040475], length 65160

Fixes: e1ededf45f07 ("rhel: Enable ERSPAN features for RHEL 7.x")
Signed-off-by: Vladislav Odintsov <odivlad@gmail.com>

acinclude.m4
@@ -671,9 +671,10 @@ AC_DEFUN([OVS_CHECK_LINUX_COMPAT], [
                         [OVS_GREP_IFELSE([$KSRC/include/net/ip_tunnels.h],
                                          [iptunnel_pull_offloads],
                         [OVS_GREP_IFELSE([$KSRC/include/net/dst_cache.h], [dst_cache],
-                        [OVS_GREP_IFELSE([$KSRC/include/net/erspan.h], [erspan_md2],
-                                         [OVS_DEFINE([USE_UPSTREAM_TUNNEL])])])])])
+                                         [OVS_DEFINE([USE_UPSTREAM_TUNNEL])])])])
 
+  OVS_GREP_IFELSE([$KSRC/include/net/erspan.h], [erspan_md2],
+                  [OVS_DEFINE([USE_BUILTIN_ERSPAN_MD2])])
   OVS_GREP_IFELSE([$KSRC/include/net/dst_cache.h], [dst_cache],
                   [OVS_DEFINE([USE_BUILTIN_DST_CACHE])])
   OVS_GREP_IFELSE([$KSRC/include/net/mpls.h], [mpls_hdr],

datapath/linux/compat/include/net/erspan.h
@@ -1,4 +1,4 @@
-#ifndef USE_UPSTREAM_TUNNEL
+#ifndef USE_BUILTIN_ERSPAN_MD2
 #ifndef __LINUX_ERSPAN_H
 #define __LINUX_ERSPAN_H
 